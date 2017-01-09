f4d6abe32b324faed109d2d2d5aacf08ddd02a24
webdevjoel <webdevjoel@outlook.com>
2017-01-09 16:05:27 -0500

    added gitignore, composer

--- php/api.php
+++ php/api.php
@@ -1,3 +1,11 @@
 <?php 
 'vendor/autoload.php';
 
+//orgs;name=Apple*
+
+$client = new GuzzleHttp\Client(['base_uri' => 'http://whois.arin.net/rest/']);
+
+// Send a request to https://foo.com/api/test
+$response = $client->request('GET', 'test');
+// Send a request to https://foo.com/root
+$response = $client->request('GET', '/root');
