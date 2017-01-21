64b1549f45feb689eaa5bd35e497a82d4e06a4ff
webdevjoel <webdevjoel@outlook.com>
2017-01-20 22:23:30 -0500

    Add Guzzle handler to force json

--- php/src/Action/Run.php
+++ php/src/Action/Run.php
@@ -3,13 +3,20 @@ namespace RestQuery\Action;
 use RestQuery\Query;
 use GuzzleHttp\Client as restClient;
 use GuzzleHttp\Promise;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+use Psr\Http\Message\RequestInterface;
 
 class Run {
     
     public static function CreateClient(Query $query){
+        $handlerStack = new HandlerStack();
+        $handlerStack->setHandler(new CurlHandler());
+        $handlerStack->push(self::add_header('Accept', 'application/json'));
+        
         $client = new restClient(
             ['base_uri' => $query->qUriFragments['base-uri']],
-            ['headers'  => ['Accept' => 'application/json']]
+            ['handler' => $handlerStack]
         );
         return $client;        
     }
@@ -18,7 +25,7 @@ class Run {
     public static function ProcessQueue(Query $query, restClient $client){
         $tempQuery = 'orgs;name=Apple*';
 
-        $response = $client->request('GET', $tempQuery);
+        $response = $client->request('GET', $tempQuery, ['headers' => ['Accept'     => 'application/json']]);
         /*$promise = $client->getAsync($tempQuery);
         
         $promise->then(function ($response) {
@@ -29,4 +36,23 @@ class Run {
         $data = $response->getBody();
         return $data;
     }
+
+    
+    //from http://docs.guzzlephp.org/en/latest/handlers-and-middleware.html
+    //this function adds middleware to a handler
+    //in this case we just want to add the application/json header
+    public static function add_header($header, $value)
+    {
+        return function (callable $handler) use ($header, $value) {
+            return function (
+                RequestInterface $request,
+                array $options
+            ) use ($handler, $header, $value) {
+                $request = $request->withHeader($header, $value);
+                return $handler($request, $options);
+            };
+        };
+    }
+
+
 }
