d5ff867afaf0a304a038d2217b4e9e65c4b2a5c1
webdevjoel <webdevjoel@outlook.com>
2017-01-25 21:37:58 -0500

    setup ready for further experimenting test commit with phpstorm

--- php/api-test.php
+++ php/api-test.php
@@ -6,31 +6,45 @@ use RestQuery\Action\Run as run;
 use RestQuery\Action\Respond as respond;
 use GuzzleHttp\Client as client;
 
+
 //test sending and receiving queries
 
+phpinfo();
 //setup
 $q = new Query();
+$q->qElements['pr'] = "Apple";
 $q->qType = 1;
 $tempQuery = 'orgs;name=Apple*';//manually define query
 
 //check setup
 print_r($q->qElements);
 //print_r($q->qRunQueue);
-
 //manually create client
 $client = new client(
             ['base_uri' => $q->qUriParts['base-uri']]
         );
 
-$response = $client->request(
-    'GET', $tempQuery, 
+$promise = $client->getAsync(
+    $tempQuery, 
     ['headers' => ['Accept'     => 'application/json']]
+)->then(
+    function ($response) {
+        $data = json_decode($response->getBody()->getContents(), true);
+        print_r($data);
+        return $data;
+    },
+    function ($e) {
+        $response = $e->getResponse();
+        print_r($response->getStatusCode());
+    }
 );
-        
-$data = $response->getBody();
-
-respond::Results($data);
 
 
-//1) fixed query, call API and print json DONE
-//
+//$data = $response->getBody();
+
+
+//respond::Results($data);
+
+
+//1 - hardcoded query, sync call API and print json WORKS
+//2 - hardcoded query, async call and print json 
